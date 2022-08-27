ff59a6c467441b079a4e9684e12b38e4dc8f9f9f
Daniil Sheftelevich <shftlvch@gmail.com>
2022-08-27 17:01:25 +0300

    improve: init more styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,9 @@ import logo from "./logo.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button></Button>
-      </header>
+    <div className="container">
+      <header></header>
+      hello
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      body: 'system-ui, -apple-system, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"'
+    },
+    container: {
+      // set padding on all media sizes to 24px
+      padding: {
+        DEFAULT: "16px",
+        lg: "24px"
+      },
+      // set max-w to 1280px
+      screens: {
+        "2xl": "1280px",
+        lg: "1024px"
+      },
+      // mx-auto automaticity
+      center: true
+    },
     extend: {}
   },
   plugins: [require("@tailwindcss/forms")]
