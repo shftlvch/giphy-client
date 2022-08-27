8a7e5164018dbd596e65a29c7088e46fe89d6776
Daniil Sheftelevich <shftlvch@gmail.com>
2022-08-27 18:27:30 +0300

    new: basic search logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Button from "@/components/Button/Button"
-import { useEffect, useState } from "react"
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
+import { Input } from "./components/Form/Input/Input"
 import Preview from "./components/Preview/Preview"
 import logo from "./logo.svg"
 import { GIF, SearchRequest } from "./types"
 import fetcher from "./utils/fetcher"
 
 function App() {
+  const [q, setQ] = useState<string>("")
   const [result, setResult] = useState<GIF[]>()
   useEffect(() => {
     const args: SearchRequest = {
-      q: "star wars",
+      q,
       offset: 0,
       limit: 9
     }
@@ -22,11 +24,22 @@ function App() {
       setResult(resp)
     }
     search()
-  }, [])
+  }, [q])
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+    setQ(e.target.value)
+  }
 
   return (
     <div className="container">
       <header></header>
+      <Input
+        name="q"
+        value={q}
+        onChange={handleSearch}
+        placeholder="Search for gifs..."
+      />
       <div className="grid grid-cols-3 gap-4">
         {result?.map((gif) => (
           <Preview key={gif.id} {...gif} />
---
+++ src/components/Form/Input/Input.tsx
@@ -0,0 +1,38 @@
+import { PropsWithCN } from "@/types"
+import { ChangeEvent, useId } from "react"
+
+type InputProps = {
+  label?: string
+  placeholder?: string
+  name: string
+  value: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+}
+
+export const Input = ({
+  label,
+  placeholder,
+  name,
+  value,
+  onChange
+}: PropsWithCN<InputProps>) => {
+  const id = useId()
+  return (
+    <div className="col-span-6 sm:col-span-3">
+      {label && (
+        <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+          {label}
+        </label>
+      )}
+      <input
+        id={id}
+        type="text"
+        className="text-black mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+        value={value}
+        name={name}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+    </div>
+  )
+}
